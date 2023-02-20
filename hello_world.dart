80fe0a129d17dbf6a81a85d7334b7ffecb7e66e2
Your Name <mmer690@rku.ac.in>
2023-02-20 13:45:06 +0530

    first commit

---
+++ hello_world.dart
@@ -0,0 +1,30 @@
+import 'dart:ffi';
+
+void main()
+{
+
+// int a=10;
+// print(a);
+
+// double b=3.45;
+// print(b);
+
+
+// bool c=true;
+//  print(c);
+
+// String name="maulik";
+// print(name);
+
+
+var name="Maulik";
+
+print("My name is $name");
+print("My name is  ${name.length}");
+
+final cityName="Mumbai";
+print(cityName);
+
+ 
+
+}
